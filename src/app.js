69f8790aa622b0b2b5ea4e0fed4d91c95edb6194
Ayush <ayushku244@gmail.com>
2026-04-01 17:37:58 +0530

    Initial setup: Express server, MongoDB, Auth, RBAC implemented

---
+++ src/app.js
@@ -0,0 +1,38 @@
+const express = require("express");
+require("dotenv").config();
+
+const connectDB = require("./config/db");
+
+const app = express();
+
+// Connect Database
+connectDB();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Finance Backend Running 🚀");
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
+const authRoutes = require("./routes/authRoutes");
+
+app.use("/api/auth", authRoutes);
+
+const { protect } = require("./middleware/authMiddleware");
+const { authorizeRoles } = require("./middleware/roleMiddleware");
+
+// Test route
+app.get(
+  "/api/test",
+  protect,
+  authorizeRoles("admin"),
+  (req, res) => {
+    res.json({ message: "Admin access granted ✅" });
+  }
+);
